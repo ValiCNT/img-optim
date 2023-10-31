ed4f31b36ee420a768640a016d62b961a837aee3
ValiCNT <104482949+ValiCNT@users.noreply.github.com>
2023-10-30 23:41:17 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Image optimization</title>
+    </head>
+    <body>
+        <img src="poze site/pexels-bekah-allmark-10433551.jpg" alt="" srcset="">
+        <img src="poze site/pexels-jaime-hernandez-erives-15509517.jpg" alt="" srcset="">
+        <img src="poze site/pexels-maryiva-12731238.jpg" alt="" srcset="">
+        <img src="poze site/pexels-nazila-azimzada-18509188.jpg" alt="" srcset="">
+        <img src="poze site/pexels-rovshan-nazirli-18565035.jpg" alt="" srcset="">
+        <img src="poze site/pexels-samira-mva-18254694.jpg" alt="" srcset="">
+        <img src="poze site/pexels-yury-gargay-14386546.jpg" alt="" srcset="">
+    </body>
+</html>
